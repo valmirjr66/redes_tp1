1fa4a6e7f2b78166c98558f9a51e8c82c9c2913a
Valmir G. M. Junior <valmir.junior.external@bt.com>
2021-12-20 22:08:38 -0300

    refactor turn wrapper

--- domain/TurnWrapper.py
+++ domain/TurnWrapper.py
@@ -6,16 +6,8 @@ from domain.NetworkInterface import NetworkInterface
 class TurnWrapper:
     def __init__(self, network_interface: NetworkInterface):
         self.content = []
-        self.turno = 0
+        self.turno = -1
         self.network_interface = network_interface
-
-        raw_turn = network_interface.get_turn(self.turno)
-
-        for item in raw_turn:
-            self.content.append(json.loads(item)['ships'])
-
-    def get_content(self):
-        return self.content
 
     def get_next_turn(self):
         self.turno += 1
@@ -24,3 +16,14 @@ class TurnWrapper:
 
         for item in raw_turn:
             self.content.append(json.loads(item))
+
+    def is_game_over(self):
+        has_game_ended = False
+
+        for item in self.content:
+            has_game_ended = has_game_ended or item['type'] == 'gameover'
+
+        return has_game_ended
+
+    def get_ships_by_river(self, river):
+        return self.content[river - 1]
