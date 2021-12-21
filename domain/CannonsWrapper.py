1d5bbe0deffcc4c73d0040bb9587a17df4d3d621
Valmir G. M. Junior <valmir.junior.external@bt.com>
2021-12-20 21:24:08 -0300

    refactor cannons wrapper to use cannon unity

--- domain/CannonsWrapper.py
+++ domain/CannonsWrapper.py
@@ -1,9 +1,24 @@
 import json
 
+from domain.CannonUnity import CannonUnity
+
 
 class CannonsWrapper:
     def __init__(self, raw_cannons):
-        self.content = json.loads(raw_cannons)['cannons']
+        self.content = []
+        for item in json.loads(raw_cannons)['cannons']:
+            self.content.append(CannonUnity(item))
 
-    def get_content(self):
-        return self.content
+    def validate_cannons(self):
+        safe_rivers_list = []
+        for item in self.content:
+            safe_rivers_list += item.get_rivers_in_range()
+
+        return len(set(safe_rivers_list)) == 4
+
+    def get_best_cannon_for_shot(self, river, bridge):
+        for item in self.content:
+            if item.get_bridge() == bridge and river in item.get_rivers_in_range():
+                return item.get_coordinates
+
+        return None
